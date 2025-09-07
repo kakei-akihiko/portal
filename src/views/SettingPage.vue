8264114f829258613325070538aa61a0e8e8fa33
掛居明彦 <kakei.akihiko@gmail.com>
2025-09-07 14:34:34 +0900

    設定画面表示調整

--- src/views/SettingPage.vue
+++ src/views/SettingPage.vue
@@ -7,43 +7,39 @@
     <template v-slot:panel-main>
       <h2>設定</h2>
 
-      <div class="card">
-        <div class="card-header">サイドバーの記事</div>
-        <div class="card-body">
-          <fieldset class="form-group">
-            <label>記事ID</label>
-            <input
-              name="sidebar-article-id"
-              class="form-control"
-              v-model="sidebarArticleId"
-            />
-          </fieldset>
-          <fieldset class="form-group mb-0" label-sr-only>
-            <button
-              class="btn btn-primary"
-              type="button"
-              @click="sidebarArticleButtonClick"
-            >
-              反映
-            </button>
-          </fieldset>
-        </div>
-      </div>
-
-      <div class="card border-danger">
-        <div class="card-header border-danger text-danger">危険な設定</div>
-        <div class="card-body">
-          <fieldset class="form-group">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              @click="deleteDatabaseButtonClick"
-            >
-              データベース削除
-            </button>
-          </fieldset>
-        </div>
-      </div>
+      <section class="sidebar-article">
+        <h3>サイドバーの記事</h3>
+        <fieldset class="form-group">
+          <label>記事ID</label>
+          <input
+            name="sidebar-article-id"
+            class="form-control"
+            v-model="sidebarArticleId"
+          />
+        </fieldset>
+        <fieldset class="form-group mb-0" label-sr-only>
+          <button
+            class="btn btn-primary"
+            type="button"
+            @click="sidebarArticleButtonClick"
+          >
+            反映
+          </button>
+        </fieldset>
+      </section>
+      
+      <section class="danger-panel">
+        <h3>危険な設定</h3>
+        <fieldset class="form-group">
+          <button
+            type="button"
+            class="delete-database"
+            @click="deleteDatabaseButtonClick"
+          >
+            データベース削除
+          </button>
+        </fieldset>
+      </section>
     </template>
   </TheMainLayout>
 </template>
@@ -73,3 +69,22 @@ onMounted(async () => {
   sidebarArticleId.value = sidebarArticleRef.value?.id
 })
 </script>
+
+<style scoped>
+.form-group {
+  border-width: 0;
+}
+
+.sidebar-article fieldset label {
+  margin-right: 10px;
+}
+
+.danger-panel {
+  margin-top: 10px;
+  border-top: 1px solid #ccc;
+}
+
+.delete-database:hover {
+  background-color: rgb(250,100,100);
+}
+</style>
