5ad78369749738de2f30f6b041d859dc8465a555
掛居明彦 <kakei.akihiko@gmail.com>
2025-11-04 08:11:19 +0900

    FullHeight.vue削除

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import router from './router'
 import App from './App.vue'
 
 import NotFoundAlert from './components/alerts/NotFoundAlert.vue'
-import FullHeight from './components/layouts/FullHeight.vue'
 import TheMainLayout from './components/layouts/TheMainLayout.vue'
 import TheSidebar from './components/sidebar/TheSidebar.vue'
 
 const app = createApp(App).use(router)
 app.component('NotFoundAlert', NotFoundAlert)
-app.component('FullHeight', FullHeight)
 app.component('TheMainLayout', TheMainLayout)
 app.component('TheSidebar', TheSidebar)
 app.mount('#app')
