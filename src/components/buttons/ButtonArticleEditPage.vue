a1808306892460833b6ef7fadfb9d5970c0b9d05
掛居明彦 <kakei.akihiko@gmail.com>
2025-11-07 20:12:53 +0900

    fix ButtonArticleEditPage

--- src/components/buttons/ButtonArticleEditPage.vue
+++ src/components/buttons/ButtonArticleEditPage.vue
@@ -8,11 +8,17 @@
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
 const props = defineProps({
   id: Number
 })
 
 const buttonClick = () => {
+  const id = props.id
+
   router.push({ name: 'ArticleEditPage', params: { id } })
 }
 </script>
